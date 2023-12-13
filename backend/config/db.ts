85aa9f6747c6aeb650cf3cb9f8c76afbf7046b79
Nitzan Maizel <nitzan@jama.co.il>
2023-12-13 21:23:17 +0200

    add modals to db and change to TS

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) throw new Error('MONGO_URI is not defined');
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
